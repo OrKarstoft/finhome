65517d3c624fdb31d210e4069aff437703443233
Oliver Karstoft <orkarstoft@gmail.com>
2025-08-30 10:26:43 +0200

    Notice alert
    Fix className

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -203,6 +203,26 @@ export default function RootLayout({
               <WelcomeModal onSelectTemplateAction={handleTemplateChoice} />
             )}
             <div className="container mx-auto p-4 sm:p-6 lg:p-8">
+              <div className="text-center py-4 lg:px-4">
+                <div
+                  className="p-2 bg-orange-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+                  role="alert"
+                >
+                  <span className="flex rounded-full bg-orange-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+                    Notice
+                  </span>
+                  <span className="font-semibold mr-2 text-left flex-auto">
+                    This is a brand new app - very much in alpha state. Bugs are
+                    being fixed continuously.
+                  </span>
+                  <svg
+                    className="fill-current opacity-75 h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  ></svg>
+                </div>
+              </div>
+              <br />
               <nav className="bg-white dark:bg-gray-800 shadow rounded-lg mb-8 dark-mode-transition">
                 <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                   <div className="relative flex items-center justify-between h-16">
