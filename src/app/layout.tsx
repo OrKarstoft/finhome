c6be0548f9d1327a61549e725cee549e5abe13b9
Oliver Karstoft <orkarstoft@gmail.com>
2025-09-03 08:55:58 +0200

    Fix navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ function InnerLayout({ children }: Readonly<{ children: React.ReactNode }>) {
         </div>
         <br />
         <nav className="bg-white dark:bg-gray-800 shadow rounded-lg mb-8 dark-mode-transition">
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="max-w-8xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center text-gray-800 dark:text-white font-bold text-xl">
